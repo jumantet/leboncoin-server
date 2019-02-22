c49fa37684dbcda1ccecb92767790f847cf3c9c1
Mantet <julian.mantet@gmail.com>
2019-02-22 14:31:22 +0100

    last modif

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cors());
+
+var helmet = require("helmet");
+app.use(helmet());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true
