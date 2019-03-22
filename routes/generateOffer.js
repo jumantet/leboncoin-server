d06bdb8a83a4c0df44a5acb804f1ed78899b1bc6
Mantet <julian.mantet@gmail.com>
2019-03-22 16:50:25 +0100

    faker offer published

---
+++ routes/generateOffer.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const Offer = require("../models/offer");
+const uploadPictures = require("../middlewares/uploadPictures");
+const faker = require("faker/locale/fr");
+
+const router = express.Router();
+
+//CREATE OFFER
+
+router.post("/offer/generate", isAuthenticated, async (req, res) => {
+  console.log("hello");
+  try {
+    for (let i = 0; i < 500; i++) {
+      const newOffer = new Offer({
+        title: faker.commerce.productName,
+        description: faker.commerce.productAdjective,
+        price: faker.commerce.price,
+        pictures: [faker.image.image, faker.image.image],
+        creator: req.user
+      });
+      const offer = Offer.findOne({ title: newOffer.title });
+      if (!offer) {
+        await newOffer.save();
+        res.json({
+          newOffer
+        });
+        i++;
+      } else {
+        i++;
+      }
+    }
+    return res.json({ message: "offers has been published" });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
